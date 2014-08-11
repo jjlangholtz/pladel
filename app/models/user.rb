c3a10b4b6ead33d0b5cf35c4d4494139f3fd2e9e
Andrew Hooge <andrewhooge@pushactive.com>
2014-08-11 13:30:06 -0400

    updated user model score

--- app/models/user.rb
+++ app/models/user.rb
@@ -97,14 +97,14 @@ class User < ActiveRecord::Base
   end
 
   def health_composite
-    nutrition = nutrition_score_to_gpa
-    sleep = sleep_score_to_gpa
-    movement = movement_score_to_gpa
+    nutrition = score_to_gpa
+    sleep = score_to_gpa
+    movement = score_to_gpa
     (nutrition + sleep + movement) / 3
   end
 
-  def nutrition_score_to_gpa
-    case nutrition_score
+  def score_to_gpa
+    case nutrition_score && sleep_score && movement_score
     when 'A'
       4.0
     when 'B'
@@ -118,35 +118,35 @@ class User < ActiveRecord::Base
     end
   end
 
-  def sleep_score_to_gpa
-    case sleep_score
-    when 'A'
-      4.0
-    when 'B'
-      3.0
-    when 'C'
-      2.0
-    when 'D'
-      1.0
-    when 'F'
-      0.0
-    end
-  end
-
-  def movement_score_to_gpa
-    case movement_score
-    when 'A'
-      4.0
-    when 'B'
-      3.0
-    when 'C'
-      2.0
-    when 'D'
-      1.0
-    when 'F'
-      0.0
-    end
-  end
+  # def sleep_score_to_gpa
+  #   case sleep_score
+  #   when 'A'
+  #     4.0
+  #   when 'B'
+  #     3.0
+  #   when 'C'
+  #     2.0
+  #   when 'D'
+  #     1.0
+  #   when 'F'
+  #     0.0
+  #   end
+  # end
+  #
+  # def movement_score_to_gpa
+  #   case movement_score
+  #   when 'A'
+  #     4.0
+  #   when 'B'
+  #     3.0
+  #   when 'C'
+  #     2.0
+  #   when 'D'
+  #     1.0
+  #   when 'F'
+  #     0.0
+  #   end
+  # end
 
   def nutrition_composite
     total_meals == 0 ? 0 : complete_meals.to_f / total_meals.to_f
