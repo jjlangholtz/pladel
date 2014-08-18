eddc0c4f8f3de874ba2d99d194d9c7ecd6b2928e
Tyler Berry <tyler.berry4@gmail.com>
2014-08-18 04:07:42 -0400

    Added animation to landing page logo, made landing page sections larger

--- app/assets/javascripts/welcome/index.js
+++ app/assets/javascripts/welcome/index.js
@@ -3,6 +3,9 @@ $(document).ready(function(){
 	$('#movement').addClass('animated fadeInLeft');
 	$('#nutrition').addClass('animated fadeInRight');
 	$('#overallDial').addClass('animated fadeInDown');
+
+	$('nav img').addClass('animated rotateIn');
+	$('nav div').addClass('animated fadeInRightBig');
 });
 
 
