6e7f3779af31fd000d3946ed18469418db8b0ca5
Joshua Langholtz <jjlangholtz@gmail.com>
2014-08-20 11:32:30 -0400

    Meal logs on dashboard now reflect only today's meals.

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,3 +1,8 @@
 class Meal < ActiveRecord::Base
   belongs_to :user
+
+  def self.today(user)
+    where('updated_at >= ? AND updated_at < ? AND user_id = ?',
+          Time.now.midnight, (Time.now + 1.day).midnight, user.id)
+  end
 end
